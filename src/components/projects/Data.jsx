f5ec3db5de09521a11d007f7a69e540c49dde6d0
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-02-10 21:37:33 +0600

    new project added

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,46 +1,45 @@
 import Nextflix from "../../assets/nextflix.webp";
-import Project2 from "../../assets/project2.webp";
+import TicketWeb from "../../assets/TicketWeb.webp";
 import Project3 from "../../assets/project3.webp";
 
-
 export const projectsData = [
-    {
-        id: 1,
-        image: Nextflix,
-        title: "Nextflix Project",
-        category: "A",
-        live:"https://nextflixproject.vercel.app/",
-        source:"https://github.com/Hrittik-Chatterjee/nextflix"
-    },
-    {
-        id: 2,
-        image: Project2,
-        title: "Project 2",
-        category: "B",
-        live:"#",
-        source:"#"
-    },
-    {
-        id: 3,
-        image: Project3,
-        title: "Project 3",
-        category: "C",
-        live:"#",
-        source:"3"
-    },
+  {
+    id: 1,
+    image: Nextflix,
+    title: "Nextflix Project",
+    category: "A",
+    live: "https://nextflixproject.vercel.app/",
+    source: "https://github.com/Hrittik-Chatterjee/nextflix",
+  },
+  {
+    id: 2,
+    image: TicketWeb,
+    title: "Ticket Website",
+    category: "B",
+    live: "https://ticketingwebapp.vercel.app/",
+    source: "https://github.com/Hrittik-Chatterjee/ticketingwebapp",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Project 3",
+    category: "C",
+    live: "#",
+    source: "3",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "A",
+  },
+  {
+    name: "B",
+  },
+  {
+    name: "C",
+  },
 ];
