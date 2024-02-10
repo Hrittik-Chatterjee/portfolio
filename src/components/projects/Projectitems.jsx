730cf981fe6c509b2a3e1a3d82765fb9fbff8860
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-02-10 21:42:50 +0600

    some small ui style fixed

--- src/components/projects/Projectitems.jsx
+++ src/components/projects/Projectitems.jsx
@@ -4,20 +4,30 @@ import { FiGithub } from "react-icons/fi";
 import { HiOutlineArrowSmRight } from "react-icons/hi";
 
 // eslint-disable-next-line react/prop-types
-const ProjectItems = ({item}) => {
+const ProjectItems = ({ item }) => {
   return (
-        <div className="project__card" key={item.id}>
-            <img className="project__img" src={item.image} alt="" />
-            <h3 className="project__title">{item.title}</h3>
-            
-            <a className="project__button" href={item.source}  target="_blank" rel="noreferrer">
-                   Source Code <FiGithub  className="project__button-icon"/>
-                </a>
-                <a href={item.live} className="project__button" target="_blank" rel="noreferrer">
-                Live Demo <HiOutlineArrowSmRight className="project__button-icon" />
-            </a>
-        </div>
-    );
-}
+    <div className="project__card" key={item.id}>
+      <img className="project__img" src={item.image} alt="" />
+      <h3 className="project__title">{item.title}</h3>
+
+      <a
+        className="project__button"
+        href={item.source}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FiGithub className="project__button-icon" /> Source Code
+      </a>
+      <a
+        href={item.live}
+        className="project__button"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <HiOutlineArrowSmRight className="project__button-icon" /> Live Demo
+      </a>
+    </div>
+  );
+};
 
 export default ProjectItems;
